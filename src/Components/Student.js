0573e54249f4d54f46a275d12a9b59c854ae9e18
akansha-1202 <120491297+akansha-1202@users.noreply.github.com>
2023-04-07 23:41:54 +0530

    second

--- src/Components/Student.js
+++ src/Components/Student.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext} from 'react'
 import Nav from './Nav'
 import { Link, useNavigate } from 'react-router-dom'
 import ContextData from './ContextAPI'
